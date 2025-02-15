2b7385d855aa4bd512d2fe34ab58c80d92f059e1
sharada0417 <sharada.kanchana2001@gmail.com>
2025-02-15 23:31:25 +0530

    add express server

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-var express = require("express");
-console.log(express)
+import express from "express"
+
+const app = express();
+
+app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+});
+
