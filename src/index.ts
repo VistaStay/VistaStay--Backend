b35907ff9a6da783e9e88ccff0edf36a52423e64
sharada417 <sharada.kanchana2001@gmail.com>
2025-04-01 18:52:30 +0530

    fix errors

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,29 @@
-import express from "express";
-import cors from 'cors';
 import 'dotenv/config';
-import paymentsRouter from "./api/payment";
+import express from "express";
 import connectDB from "./infastructure/db";
-import hotelRouter from "./api/hotel";
-import userRouter from "./api/user";
-import bookingRouter from "./api/booking";
-import bodyParser from "body-parser";
-import globalErrorHandlingMiddleware from "./api/middlewares/global-error-handlig-middleware";
 import { clerkMiddleware } from "@clerk/express";
+import cors from 'cors';
+import bookingRouter from "./api/booking";
+import hotelRouter from "./api/hotel";
+import globalErrorHandlingMiddleware from "./api/middlewares/global-error-handlig-middleware";
 import { handleWebhook } from "./application/payment";
+import bodyParser from "body-parser";
+import paymentsRouter from "./api/payment";
+
 const app = express();
-app.use(express.json());
 app.use(clerkMiddleware());
-
 app.use(cors({ origin: process.env.FRONTEND_URL }));
-
-connectDB();
 app.post(
   "/api/stripe/webhook",
   bodyParser.raw({ type: "application/json" }),
   handleWebhook
 );
+app.use(express.json());
 app.use("/api/hotels",hotelRouter)
-app.use("/api/users",userRouter)
 app.use("/api/bookings",bookingRouter)
 app.use("/api/payments", paymentsRouter);
 app.use(globalErrorHandlingMiddleware)
-
+connectDB();
 app.listen(8083, () => {
   console.log("server is running on port 8083");
 });
